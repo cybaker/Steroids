5f14a559936cf5445c2c9977cc8b2cf94f34ffe4
Thomas Baker <thomas_baker@intuit.com>
2022-05-25 06:45:28 -0700

    Polygon asteroid game bounds fix

--- lib/src/game/components/game_edge.dart
+++ lib/src/game/components/game_edge.dart
@@ -3,40 +3,38 @@ import 'dart:ui';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/palette.dart';
+import 'package:steroids/src/game/components/polygonAsteroid.dart';
+import 'package:steroids/src/game/extensions/component_effects.dart';
 
 import '../../level_selection/levels.dart';
 import '../steroids.dart';
 
-class GameEdge extends PolygonComponent
-    with HasGameRef<SteroidsLevel>,
-        CollisionCallbacks  {
+class GameEdge extends PolygonComponent with HasGameRef<SteroidsLevel>, CollisionCallbacks {
   final GameLevel level;
 
-  GameEdge({required this.level}) : super([
-    Vector2(level.playfieldDimension.toDouble(), level.playfieldDimension.toDouble()),
-    Vector2(level.playfieldDimension.toDouble(), -level.playfieldDimension.toDouble()),
-    Vector2(-level.playfieldDimension.toDouble(), -level.playfieldDimension.toDouble()),
-    Vector2(-level.playfieldDimension.toDouble(), level.playfieldDimension.toDouble()),
-  ]);
+  GameEdge({required this.level})
+      : super([
+          Vector2(level.playfieldDimension.toDouble(), level.playfieldDimension.toDouble()),
+          Vector2(level.playfieldDimension.toDouble(), -level.playfieldDimension.toDouble()),
+          Vector2(-level.playfieldDimension.toDouble(), -level.playfieldDimension.toDouble()),
+          Vector2(-level.playfieldDimension.toDouble(), level.playfieldDimension.toDouble()),
+        ]);
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     paint = BasicPalette.red.paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 5;
+      ..strokeWidth = 20;
     await add(PolygonHitbox(vertices));
   }
 
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
-    // debugPrint('Edge Collided with $other');
-    var offset = level.playfieldDimension.toDouble() - 20;
-    if (other.position.x.abs() >= level.playfieldDimension.toDouble()) {
-      other.position.x = other.position.x > 0 ? -offset : offset;
-    } else if (other.position.y.abs() >= level.playfieldDimension.toDouble()) { // y must be hitting
-      other.position.y = other.position.y > 0 ? -offset : offset;
-    }
     super.onCollision(intersectionPoints, other);
+    // debugPrint('Edge Collided with $other');
+    if (!(other is PolygonAsteroid)) {
+      other.keepWithinGameBounds(level);
+    }
   }
 }
--- lib/src/game/extensions/component_effects.dart
+++ lib/src/game/extensions/component_effects.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
+import 'package:steroids/src/level_selection/levels.dart';
 
 extension ComponentExtension on Component {
   void shake() {
@@ -8,5 +9,20 @@ extension ComponentExtension on Component {
       ScaleEffect.by(Vector2(1.1, 1.1), EffectController(duration: 0.1, reverseDuration: 0.1, repeatCount: 1)),
     ]);
     add(effect);
+  }
+}
+
+extension PositionComponentExtension on PositionComponent {
+  void keepWithinGameBounds(GameLevel level) {
+    if (position.x >= level.playfieldDimension) {
+      position.x = -level.playfieldDimension + 10;
+    } else if (position.x <= - level.playfieldDimension) {
+      position.x = level.playfieldDimension -10;
+    }
+    if (position.y >= level.playfieldDimension) {
+      position.y = -level.playfieldDimension + 10;
+    } else if (position.y <= - level.playfieldDimension) {
+      position.y = level.playfieldDimension -10;
+    }
   }
 }
